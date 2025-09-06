6bd167c3e82ec736aaa156ae5effce131dac9316
SeraphKizhin <lodivesekenji@gmail.com>
2025-09-07 00:45:03 +0800

    Update Game.java
    Added user input for actual gameplay

--- game/Game.java
+++ game/Game.java
@@ -1,14 +1,48 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
-        Player Seraph = new Player("Seraph");
-        Dice player1 = new Dice(Seraph);
+        Scanner sc = new Scanner(System.in);
+        char op;
 
-        player1.roll(3);
+        System.out.print("Enter player name: ");
+        String name = sc.nextLine();
+        Player player1 = new Player(name);
+        Dice playerDice1 = new Dice(player1);
 
-        Player Kizhin = new Player("Kizhin");
-        Dice player2 = new Dice(Kizhin);
+        do {
+            System.out.print("Op: ");
 
-        player2.roll(2);
+            op = sc.next().charAt(0);
+
+            switch (op) {
+                case 'r':
+                    System.out.println("Rolling...");
+                    playerDice1.roll(3);
+                    break;
+                case 'f':
+                    Roaming roaming = new Roaming(player1);
+                    System.out.println("Roaming...");
+                    roaming.roam();
+                    break;
+                case 'q':
+                    System.out.println("Returning to village...");
+                    break;
+                default:
+                    System.out.println("Unknown Operation");
+            }
+        } while (op != 'q');
+        
+
+        // Player Seraph = new Player("Seraph");
+        // Dice player1 = new Dice(Seraph);
+
+        // player1.roll(3);
+
+        // Player Kizhin = new Player("Kizhin");
+        // Dice player2 = new Dice(Kizhin);
+
+        // player2.roll(2);
     
     }
 }
