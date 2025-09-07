1f9fce24e640bd418dc998c194904743249a5892
SeraphKizhin <lodivesekenji@gmail.com>
2025-09-07 21:44:57 +0800

    Update Roaming.java

--- game/Roaming.java
+++ game/Roaming.java
@@ -20,17 +20,32 @@ public class Roaming {
     }
 
     public void scenario(){
-        System.out.println("As you walk through the forest you find a gentleman asking for chump change. What do you do?\n\tSeduce\tor\tDevour");
+        System.out.println("As you walk through the forest you find a gentleman asking for chump change. What do you do?");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+        System.out.println("\tSeduce\tor\tDevour");
         String choice = sc.nextLine();
         if(choice.equals("Seduce")){
-            System.out.println("The gentlSeman grunts and lets you pass through.");
+            System.out.println("The gentleman grunts and lets you pass through.");
         } else if (choice.equals("Devour")){
             fight();
         } 
     }
 
     public void fight(){
-        System.out.println("You pounced on a bandit!\nEntering combat...");
+        System.out.println("You pounced on a bandit!");
+        System.out.print("Engaging combat");
+        for(int i = 0; i < 3; i++){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.print(".");
+        }
+        System.out.println();        
         Enemy bandit = Enemy.monsterChoose(1);
         Fight battle = new Fight(this.player, bandit);
         battle.battle(this.player, bandit);
