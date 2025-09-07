bac31afb533d931edc6b69b6ad0cdad046807f9a
SeraphKizhin <lodivesekenji@gmail.com>
2025-09-07 21:43:51 +0800

    Update Fight.java

--- game/Fight.java
+++ game/Fight.java
@@ -4,34 +4,15 @@ public class Fight {
     Random rand = new Random();
     private final Player player;
     private final Enemy name;
+    private Dice dice;
 
     public Fight(Player player, Enemy name){
         this.player = player;
         this.name = name;
+        this.dice = new Dice();
     }
 
     public void battle(Player player, Enemy name){
-        int playerRoll;
-        int monsterRoll;
-        System.out.println(player + " rolling... ");
-        playerRoll = calculation();
-        System.out.println(playerRoll);
-        System.out.println(name + " rolling... ");
-        monsterRoll = calculation();
-        System.out.println(monsterRoll);
-        if(playerRoll > monsterRoll){
-            System.out.println("Player wins!");
-        } else if (monsterRoll > playerRoll) {
-            System.out.println("Player loses...");
-        } else if (playerRoll == monsterRoll) {
-            System.out.println("Clash!");
-            battle(player, name);
-        }
+        dice.clash(name, player);
     }
-
-    public int calculation(){
-        int rolls = rand.nextInt(21) + 1;
-        return rolls;
-    }
-    
 }
