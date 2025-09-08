2f2b4debd7dd52e6dd7a1579e0fbbcb2bc80e74b
SeraphKizhin <lodivesekenji@gmail.com>
2025-09-08 22:49:25 +0800

    Update Fight.java
    added UI and user decision

--- game/Fight.java
+++ game/Fight.java
@@ -1,7 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Fight {
     Random rand = new Random();
+    Scanner sc = new Scanner(System.in);
     private final Player player;
     private final Enemy name;
     private Dice dice;
@@ -13,6 +15,24 @@ public class Fight {
     }
 
     public void battle(Player player, Enemy name){
-        dice.clash(name, player);
+        Fight fight = new Fight(player, name);
+        System.out.print("Fight \t\t\t Run\nItem \t\t\t Talk\n// ");
+        String action = sc.nextLine();
+        action = action.toLowerCase();
+        if(action.equals("fight")){
+            dice.clash(name, player);
+        } else if (action.equals("run")){
+            int run = rand.nextInt(2);
+            if(run == 1){
+                System.out.println("You successfully escaped.");
+            } else {
+                System.out.println("You failed to escape.");
+                // take damage here
+                fight.battle(player, name);
+            }
+        } else {
+            System.out.println("Unknown command.");
+            fight.battle(player, name);
+        }
     }
 }
