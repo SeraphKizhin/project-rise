88aa0eeab4d88d7131d7a0486dd0ac0086f9bccd
SeraphKizhin <lodivesekenji@gmail.com>
2025-09-09 22:36:43 +0800

    Update StoryPrinter.java
    suppressed warnings

--- game/StoryPrinter.java
+++ game/StoryPrinter.java
@@ -1,11 +1,13 @@
 public class StoryPrinter {
     
+    @SuppressWarnings("BusyWait")
     public static void StoryTyper (String text, int delayMillis) {
         for (char c : text.toCharArray()){
             System.out.print(c);
             try {
                 Thread.sleep(delayMillis);
-            } catch (Exception e) {
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
         System.out.println();
