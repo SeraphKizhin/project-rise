d10b05c363d58658843b5cf7c3c578935ff3fce4
SeraphKizhin <lodivesekenji@gmail.com>
2025-09-10 18:09:00 +0800

    Update Fight.java
    cleaned it out, and added the clash method from dice class

--- game/Fight.java
+++ game/Fight.java
@@ -1,40 +1,110 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Fight {
     Random rand = new Random();
     Scanner sc = new Scanner(System.in);
-    private final Player player;
-    private final Enemy name;
     private final Dice dice;
 
     public Fight(Player player, Enemy name){
-        this.player = player;
-        this.name = name;
         this.dice = new Dice();
     }
 
     public void battle(Player player, Enemy name){
-        Fight fight = new Fight(player, name);
         System.out.println();
-        System.out.print("Fight \t\t\t Run\nItem \t\t\t Talk\n// ");
-        String action = sc.nextLine();
-        action = action.toLowerCase();
+        System.out.print("[0]Fight \t\t\t[1]Run\n[2]Item \t\t\t[3]Talk\n// ");
+        int action = sc.nextInt();
         System.out.println();
-        if(action.equals("fight")){
-            dice.clash(name, player);
-        } else if (action.equals("run")){
-            int run = rand.nextInt(2);
-            if(run == 1){
-                System.out.println("You successfully escaped.");
-            } else {
-                System.out.println("You failed to escape.");
-                player.takeDamage(name.getAtk());
-                fight.battle(player, name);
+        switch (action) {
+            case 0 -> clash(name, player);
+            case 1 -> {
+                int run = rand.nextInt(2);
+                if(run == 1){
+                    System.out.println("You successfully escaped.");
+                } else {
+                    System.out.println("You failed to escape.");
+                    player.takeDamage(name.getAtk());
+                    this.battle(player, name);
+                }
             }
-        } else {
-            System.out.println("Unknown command.");
-            fight.battle(player, name);
+            default -> {
+                System.out.println("You don't know what that does...");
+                this.battle(player, name);
+            }
+        }
+    }
+
+    @SuppressWarnings("BusyWait")
+    public void clash(Enemy enemy, Player player){
+        while(player.getDiceAmount() != 0 && enemy.getDiceAmount() != 0){
+            // Player Roll
+            System.out.print(player.getName() + " rolling");
+            for(int i = 0; i < 3; i++){
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+                System.out.print(".");
+            }
+            System.out.println();    
+            int playerRoll = dice.playerRoll(player);
+
+            // Enemy Roll
+            System.out.print(enemy.getName() + " rolling");
+            for(int i = 0; i < 3; i++){
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+                System.out.print(".");
+            }
+            System.out.println(" ");    
+            int enemyRoll = dice.enemyRoll(enemy);
+
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+
+            // Dice Compare
+             if(playerRoll > enemyRoll){
+                 enemy.setDiceAmount(enemy.getDiceAmount() - 1);
+                 System.out.println(enemy.getName() + " is staggered! (-1 Dice)");
+             } else if (playerRoll < enemyRoll){
+                 player.setDiceAmount(player.getDiceAmount() - 1);
+                 System.out.println(player.getName() + " is staggered! (-1 Dice)");
+             } else if(playerRoll == enemyRoll){
+                 System.out.println("Clash!");
+             }    
+        }
+        
+        // Damage and Death
+        if (player.getDiceAmount() == 0){
+            player.takeDamage(enemy.getAtk());
+            player.resetDice();
+            enemy.resetDice();
+            this.battle(player, enemy);
+        }
+
+        if(enemy.getDiceAmount() == 0){
+            int damage = player.getAttack();
+
+            if(player.getDiceAmount() >= 3){
+                damage *= 2;
+                System.out.println("You overwhelmed the enemy!");
+            } 
+            if(rand.nextDouble() < player.getCritChance()) {
+                damage *= 2;
+                System.out.println("You hit the " + enemy.getName() +"'s vital organs!");
+            }
+
+            enemy.takeDamage(damage);
+
+            player.resetDice();
+            enemy.resetDice();
+            if(enemy.isAlive()) this.battle(player, enemy);
         }
     }
 }
